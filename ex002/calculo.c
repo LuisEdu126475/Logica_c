cbd716f570bf7afb82b5592e61016fb1cabb1bcb
Devil <Alucard66khs@gmail.com>
2026-09-16 16:08:59 -0300

    Uso das operacoes matematicas

---
+++ ex002/calculo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    printf("%d + %d = %d\n\t", 10, 15, 10 + 15);
+    printf("%d - %d = %d\n\t", 10, 15, 10 - 15);
+    printf("%d * %d = %d\n\t", 10, 15, 10 * 15);
+    printf("%d / %d = %d\n\t", 10, 15, 10 / 15);
+    return 0;
+}
